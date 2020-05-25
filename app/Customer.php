5709ae056b2f1df4e3ca625c969781cb2a2fcfc7
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-05-25 16:39:47 +0200

    Add customer model and db migration

---
+++ app/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name'
+    ];
+}
