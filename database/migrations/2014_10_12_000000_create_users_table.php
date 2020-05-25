0f85f5b3c62ed8bb6b5e8c12a45eb1a000773be4
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-05-25 16:16:51 +0200

    Update db migration for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name_first');
+            $table->string('name_last');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->bigInteger('customer_id')->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
         });
     }
 
