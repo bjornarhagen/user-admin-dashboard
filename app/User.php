cff18689b55e02611af82a7af64f554f32b8cf25
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-05-26 16:57:44 +0200

    Add validator to user model

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
@@ -12,8 +14,29 @@ class User extends Model
         'name_first', 'name_last', 'email', 'phone_number'
     ];
 
+    // Combine first and last name
     public function getNameAttribute()
     {
         return $this->name_first . ' ' . $this->name_last;
+    }
+
+    public function validate()
+    {
+        return self::validator($this->toArray())->validate();
+    }
+
+    public static function validator(array $data)
+    {
+        // Get a comma seperated list of all customer ids
+        $customer_ids = Customer::pluck('id')->toArray();
+        $customer_ids = implode(',', $customer_ids);
+
+        return Validator::make($data, [
+            'name_first' => ['required', 'string'],
+            'name_last' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'phone_number' => ['required', 'string'],
+            'customer_id' => ['required', 'in:' . $customer_ids],
+        ]);
     }
 }
