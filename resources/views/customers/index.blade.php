161403594cbffd01d188e21efcb05b992c8dc8ca
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-05-27 08:39:06 +0200

    Add message to customers listing when there's no customers

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -6,6 +6,9 @@
             <strong>{{ __('Customers') }}</strong>
         </div>
         <ul class="list-group list-group-flush">
+            @if (count($customers) == 0)
+                <li class="list-group-item">{{ __('No customers found') }}</li>
+            @endif
             @foreach ($customers as $customer)
             <li class="list-group-item">
                 <a href="{{ route('user.index', $customer) }}">{{ $customer->name }}</a>
